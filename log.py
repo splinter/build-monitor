dc711c400c31ca74b60c6e8ae218a5990aa6784b
splinter <mgjaye@gmail.com>
2023-10-27 14:00:09 +1000

    Corrected timestamp generation

--- log.py
+++ log.py
@@ -1,22 +1,27 @@
 import datetime
 import logging
 
+def get_current_timestamp():
+    return datetime.datetime.timestamp(datetime.datetime.now())
+
 def add_time(logMessage):
-    now = datetime.datetime.now().total_seconds()
+    now = get_current_timestamp()
     if(logMessage.index("timedata") < 0):
         logMessage += "timedata "
     logMessage +=str(now)
     return logMessage
 
 def debug_time_info(logMessage):
-    now = datetime.datetime.now().total_seconds()
+    now = get_current_timestamp()
     components = logMessage.split("timedata")
     timestamps = components[1].split(" ")
     last = now
     details=""
     lastHop= "current"
     for i in range(len(timestamps),0):
-        diff = last - int(timestamps[i].strip())
+        time = datetime.datetime.fromtimestamp(timestamps[i])
+        diff = last - time
         details=" hop "+ lastHop +" to hop" +  i + " = " + diff +"s "
-        last = int(timestamps[i].strip())
+        last = time
+        lastHop=i
     logging.info(details)
