935b1041b4832cc60b38b62424c6fea8b102aa13
splinter <mgjaye@gmail.com>
2023-10-28 21:34:52 +1000

    Fixed

--- log.py
+++ log.py
@@ -15,17 +15,19 @@ def add_time(logMessage):
 
 def debug_time_info(logMessage):
     now = get_current_timestamp()
+    logMessage+=str(now) + " "
     components = logMessage.split("timedata")
     print(components)
     timestamps = components[1].strip().split(" ")
     last = now
     details=""
-    lastHop= "current"
+    lastHop= "0"
     
     for i in range(len(timestamps)-2,0,-1):
         time = datetime.datetime.fromtimestamp(float(timestamps[i]))
         diff = (datetime.datetime.fromtimestamp(last) - time).total_seconds()
-        details =details + " hop "+ str(lastHop) +" to hop" +  str(i) + " = " + str(diff) +"s "
+        details = details + " hop "+ str(lastHop) +" to hop " +  str(i) + " = " + str(diff) +"s "
         last = time
         lastHop=i
+    details = details
     logging.info(details)
